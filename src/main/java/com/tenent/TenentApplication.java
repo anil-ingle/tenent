0edefec8772e064779aa18b828ac96acdc301857
anil <anilingle91@mail.com>
2018-09-23 09:05:53 +0530

    project dir created:

---
+++ src/main/java/com/tenent/TenentApplication.java
@@ -0,0 +1,12 @@
+package com.tenent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tenent/TenentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tenent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TenentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
